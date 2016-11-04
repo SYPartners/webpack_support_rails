58fe32240bf69045b23be1c987917db87e296eee
Erik Belusic <erikbelusic@outlook.com>
2016-11-04 14:28:38 -0400

    Rename generator class

--- lib/generators/webpack_support_rails/initializer_generator.rb
+++ lib/generators/webpack_support_rails/initializer_generator.rb
@@ -1,7 +1,7 @@
 require 'rails/generators'
 
 module WebpackSupportRails
-  class InstallGenerator < Rails::Generators::Base
+  class InitializerGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
 
     desc 'Webpack Support Rails installation generator'
