fc1ff8b75194e95f7b62b1272cf003e4a11bdf58
Erik Belusic <erikbelusic@outlook.com>
2017-04-05 10:26:12 -0400

    Set defaults in WebpackSupportRails module

--- lib/webpack_support_rails.rb
+++ lib/webpack_support_rails.rb
@@ -2,5 +2,17 @@ require "webpack_support_rails/version"
 require 'webpack_support_rails/railtie' if defined?(Rails)
 
 module WebpackSupportRails
-  mattr_accessor :update_webpack_manifest_every_request, :bypass_inline_webpack
+  # Forces the webpack manifest to be updated on every page request.
+  # Set to true for development
+  # Set to false in production to increase response speed
+  # Default: !Rails.env.production?
+  mattr_accessor :update_webpack_manifest_every_request
+  @@update_webpack_manifest_every_request = !Rails.env.production?
+
+  # When set to true, all js bundles will be loaded externally instead of inlined.
+  # Set to true for development
+  # Set to false in production to inline javascript
+  # Default: !Rails.env.production?
+  mattr_accessor :bypass_inline_webpack
+  @@bypass_inline_webpack = !Rails.env.production?
 end
